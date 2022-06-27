9b5dff8fa526c2f623edf337cb0a753cb238eaeb
Samuel Ferracini <samferracini@gmail.com>
2022-06-26 23:35:32 -0300

    feat: create turn message component

---
+++ src/components/TurnMessage.tsx
@@ -0,0 +1,9 @@
+import styles from "./TurnMessage.module.css";
+
+export function TurnMessage() {
+  return (
+    <div className={styles.wrapper}>
+      <h3>Time Azul Joga</h3>
+    </div>
+  );
+}
