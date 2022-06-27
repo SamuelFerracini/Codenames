690027445f04c7d35459d9c47299d681772cd738
Samuel Ferracini <samferracini@gmail.com>
2022-06-26 23:35:57 -0300

    refactor: improve card css to be responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import styles from "./App.module.css";
 import { Board } from "./components/Board";
 
 import { ETeamId, TeamBar } from "./components/TeamBar";
+import { TurnMessage } from "./components/TurnMessage";
+import { WordInput } from "./components/WordInput";
 
 function App() {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.layout}>
       <TeamBar teamId={ETeamId.BLUE} />
-      <Board />
+
+      <div className={styles.wrapper}>
+        <TurnMessage />
+        <Board />
+        <WordInput />
+      </div>
+
       <TeamBar teamId={ETeamId.RED} />
     </div>
   );
